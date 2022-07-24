3204330fe34e982787b63264833e6ba04c479f7b
Raven <revanfauzi81@gmail.com>
2022-07-24 15:03:32 +0700

    First Deploy

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+const {
+  addNewNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+// // cors
+// {
+//   options: {
+//     cors: {
+//       origin: ['*'],
+//     }
+//   }
+// }
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNewNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
